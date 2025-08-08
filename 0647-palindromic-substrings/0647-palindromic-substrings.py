16b8310ff6b25ac41f195429f86f2ff8fa17010a
Dirshaye  Faltamo <212802094@ogr.cbu.edu.tr>
2025-08-08 10:12:42 +0100

    Time: 199 ms (31.22%), Space: 25.7 MB (12.1%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,16 +3,14 @@ class Solution:
         n = len(s)
         dp = [[False]* n for _ in range(n)]
         count = 0
-        '''
         for i in range(n):
             dp[i][i] = True
             count +=1
-        '''
 
         for end in range(n):
-            for begin in range(end+1):
+            for begin in range(end):
                 if s[begin] == s[end]:
-                    if end - begin <= 1 or dp[begin+1][end-1]:
+                    if end - begin == 1 or dp[begin+1][end-1]:
                         dp[begin][end] = True
                         count +=1
         return count
